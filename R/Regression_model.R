4155a28c93318380060b9f30c0858d8289a1cdf0
unknown <hannes.holstein0@googlemail.com>
2022-04-02 21:34:08 +0200

    #edit surface

--- R/Regression_model.R
+++ R/Regression_model.R
@@ -4,13 +4,25 @@ library(Metrics)
 library(car)
 
 
+#Data from Excel - preparation details see excel file in comments
+CPI <- read.csv("C:\\Users\\Hannes Holstein\\Documents\\R\\CPI.csv",sep=";")
+CPI_data <- do.call(cbind.data.frame, CPI)
+BTC <- read.csv("C:\\Users\\Hannes Holstein\\Documents\\R\\btc.csv",sep=";")
+ETH <- read.csv("C:\\Users\\Hannes Holstein\\Documents\\R\\eth.csv",sep=";")
+Gold <- read.csv("C:\\Users\\Hannes Holstein\\Documents\\R\\gold.csv",sep=";")
+REIT <- read.csv("C:\\Users\\Hannes Holstein\\Documents\\R\\REIT.csv",sep=";")
+Six_M_Bill <- read.csv("C:\\Users\\Hannes Holstein\\Documents\\R\\6M_bill.csv",sep=";")
+SP500 <- read.csv("C:\\Users\\Hannes Holstein\\Documents\\R\\sp500.csv",sep=";")
+
+
 ##NOTICE: right now regression will not work due to data fitting bugs - to be resolved asap
 #OLS-regression
-ols_model <- lm(series_CPI_df$value~series_reit_df$value + series_treasury_df + series_sp500_df + series_gold_df + coin_hist_btc + coin_hist_eth)
+ols_model <- lm(CPI$CPIAUCSL_PC1~BTC$Moving.1.Month.Average + Gold$AM + REIT$Moving.1.Month.Average + Six_M_Bill$Moving.1.Month.Average +  SP500$Moving.1.Month.Average)
 
 
-
-
+lm(series_CPI_df$value~series_reit_df$value + series_treasury_df + series_sp500_df + series_gold_df + coin_hist_btc + coin_hist_eth)
+#CPI less food & energy
+ols_CPI_LFE
 
 # Checking model statistics
 summary(ols_model)
